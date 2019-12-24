a9e0699ee7331c412059f3a6f1511e084d2d99b9
Karla Hernandez <karla@Karlas-MacBook-Pro.local>
2019-12-23 23:31:16 -0800

    cleaned up HTML

--- scare-starlings/script.js
+++ scare-starlings/script.js
@@ -22,7 +22,7 @@ var Starling = function (id, scarea) {
   // (positions closer to the center of the page are more common)
   var angle = Math.random()*2*Math.PI;
   var range = Math.pow(Math.random(),4)*initRad;
-  
+
   this.x = Math.floor(Math.cos(angle)*range + w/2);
   this.y = Math.floor(Math.sin(angle)*range + h/2);
 
@@ -31,9 +31,9 @@ var Starling = function (id, scarea) {
   this.baseSpeed = Math.random()*0.5 + .5;
   this.speed = this.baseSpeed;
   this.alpha = this.baseSpeed;
-  this.color = '#262626';
+  this.color = '#0D0D0D';
   this.id = id;
-  
+
   this.scareDist = 0.1*Math.random() + scarea;
   this.fearFactor = Math.random()*0.2 + 0.8;
   this.goalFactor = Math.random()*0.1 + 0.9;
@@ -62,7 +62,7 @@ function draw() {
   ctx.clearRect(0, 0, w, h);
 
   for (i = 0; i < Starlings.length; i++) {
-    
+
     ctx.fillStyle = Starlings[i].color;
     ctx.globalAlpha = Starlings[i].alpha;
 
@@ -81,7 +81,7 @@ function draw() {
 
 // Function for updating Starling positions
 function updateStarlingPosition(Starling){
-  
+
   // Wrap a Starling's position if it goes off the edge
   if (Starling.x < 0){
     Starling.x += w;
@@ -95,66 +95,66 @@ function updateStarlingPosition(Starling){
   else if (Starling.y > h){
     Starling.y -= h;
   }
-  
+
   var angle_to_center = Math.atan2(h/2 - Starling.y,
                                    w/2 - Starling.x);
   var distance_center = Math.sqrt(Math.pow(w/2 - Starling.x, 2) +
-                                  Math.pow(h/2 - Starling.y, 2));  
-  
+                                  Math.pow(h/2 - Starling.y, 2));
+
   // If 'mousePos' exists and is not null
   if (mousePos!==null && typeof mousePos !== 'undefined') {
 
     var angle_away_mouse = Math.PI +
         Math.atan2(mousePos.y - Starling.y,
                    mousePos.x - Starling.x);
-    var distance_mouse = 
+    var distance_mouse =
         Math.sqrt(Math.pow(mousePos.y - Starling.y, 2) +
                   Math.pow(mousePos.x - Starling.x, 2));
 
     // If Starling within 'Starling.scareDist' of 'mousePos'
     if (distance_mouse <= Starling.scareDist) {
-      
-      Starling.direction = 
-        Starling.direction*(1 - Starling.fearFactor) + 
+
+      Starling.direction =
+        Starling.direction*(1 - Starling.fearFactor) +
         angle_away_mouse*(Starling.fearFactor);
-      
-      Starling.speed = 
+
+      Starling.speed =
         Starling.speed*(1 - Starling.fearFactor) +
-        Starling.fearFactor*(maxSpeed*(1 -distance_mouse/Starling.scareDist) + 
+        Starling.fearFactor*(maxSpeed*(1 -distance_mouse/Starling.scareDist) +
         Starling.baseSpeed*(distance_mouse/Starling.scareDist));
-      
+
       // If Starling not within 'Starling.scareDist'  from 'mousePos'
     }  else if (distance_center > initRad &&
                 distance_mouse >= Starling.scareDist*(1 + 0.5)) {
-    
+
         Starling.direction = Starling.direction*(1 - Starling.goalFactor) +
           angle_to_center*(Starling.goalFactor);
-        
+
         Starling.speed = Starling.speed*(1 - Starling.goalFactor) +
         Starling.goalFactor*(maxSpeed*(1 - initRad/distance_center) +
                               Starling.baseSpeed*(initRad/distance_center));
     }
-    
+
     // If 'mousePos' does not exist or is null
   } else {
-    
+
     // If Starling far from center of page
     if (distance_center > initRad){
         Starling.direction = Starling.direction*(1 - Starling.goalFactor) +
           angle_to_center*(Starling.goalFactor);
-        
+
         Starling.speed = Starling.speed*(1 - Starling.goalFactor) +
         Starling.goalFactor*(maxSpeed*(1 - initRad/distance_center) +
                               Starling.baseSpeed*(initRad/distance_center));
       } else {
-        
+
         Starling.speed = Starling.speed*(1 - Starling.normalFactor) +
           Starling.baseSpeed*(Starling.normalFactor);
       }
   }
-  
+
   // Updating Starling's heading (direction)
-  var addX = Math.cos(Starling.direction) * Starling.speed; 
+  var addX = Math.cos(Starling.direction) * Starling.speed;
   var addY = Math.sin(Starling.direction) * Starling.speed;
 
   Starling.x = Starling.x + addX;
